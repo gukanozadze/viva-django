10f63cd7757d83745797cd1c52f8e36c5dd386cd
guka nozadze <gukanozadze@gmail.com>
2019-01-26 15:53:27 +0400

    switchet to postgres db

--- django_project/settings.py
+++ django_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vivadb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgresdjango4321',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
